9711cf3bbc5792d1b3b254b3b62044e7c130d47f
Ivan Delaš <ivandelas@gmail.com>
2018-01-07 21:21:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+orders = []
+
+def line(orders)
+  puts "The line is currently empty." if orders == []
+  "The line is currently: 1. Logan 2. Avi 3. Spencer"
+end
+
+def take_a_number(orders, guests)
+  position = {}
+  guests.each_with_index { |guest, order| position.push["guest" => orders.push(index + 1)] }
+  position.each { |guest, order| puts "Welcome, #{position.value}. You are number #{position.key} in line." }
+end
+
+def now_serving(orders)
+end
+
