bd82d57cd6a9d2d7bab6c31dcfba0da22912caff
Ivan Delaš <ivandelas@gmail.com>
2018-01-07 21:21:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ orders = []
 
 def line(orders)
   puts "The line is currently empty." if orders == []
-  "The line is currently: 1. Logan 2. Avi 3. Spencer"
+  #{}"The line is currently: 1. Logan 2. Avi 3. Spencer"
 end
 
 def take_a_number(orders, guests)
@@ -13,4 +13,3 @@ end
 
 def now_serving(orders)
 end
-
