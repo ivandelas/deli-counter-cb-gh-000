33c1588718b34078d696610d2eea68f248684592
Ivan Delaš <ivandelas@gmail.com>
2018-01-07 21:23:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ end
 def take_a_number(orders, guests)
   position = {}
   guests.each_with_index { |guest, index| position.push["guest" => orders.push(index + 1)] }
-  position.each { |guest, order| puts "Welcome, #{position.value}. You are number #{position.key} in line." }
+  position.each { |guest, index| puts "Welcome, #{position.value}. You are number #{position.key} in line." }
 end
 
 def now_serving(orders)
